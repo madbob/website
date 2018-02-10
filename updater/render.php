7301f939c2d155fe92842500c90c545b13a2a248
Roberto Guido <bob4job@gmail.com>
2018-02-10 01:06:57 +0100

    ritocchi estetici

--- updater/render.php
+++ updater/render.php
@@ -13,17 +13,17 @@ foreach($files as $file) {
 		continue;
 
 	$data = json_decode(file_get_contents($file));
-	
-	$output .= sprintf('<div class="col-md-6">' . "\n");
+
+	$output .= sprintf('<div class="col-md-4">' . "\n");
 	$output .= sprintf('	<div class="project %s" style="background-image: url(img/%s)">' . "\n", $data->type, $data->image);
 	$output .= sprintf('		<div class="overlay">' . "\n");
 	$output .= sprintf('			<span class="desc">' . "\n");
-	$output .= sprintf('				<h2>%s</h2' . "\n", $data->name);
+	$output .= sprintf('				<h4>%s</h4>' . "\n", $data->name);
 	$output .= sprintf('				<p>%s</p>' . "\n", $data->description);
 	$output .= sprintf('				<p>' . "\n");
 
 	foreach($data->links as $link)
-		$output .= sprintf('				<a href="%s">%s</a>' . "\n", $link->url, $link->name);
+		$output .= sprintf('				<a target="_blank" href="%s">%s</a>' . "\n", $link->url, $link->name);
 
 	$output .= sprintf('				</p>' . "\n");
 	$output .= sprintf('			</span>' . "\n");
